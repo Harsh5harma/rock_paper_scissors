5c275ca7b3b23211ae3363aa75342c425408e4d6
Harsh <harshsharma41664@gmail.com>
2022-10-01 07:52:39 +0530

    made the game run for 5 rounds

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ function getComputerChoice(){
     let puterChoice = Math.floor(Math.random()*3);
     return puterChoice;
 }
-let cc=getComputerChoice();
-let uc = prompt("Enter your choice");
-uc = uc.toLowerCase();
-
 function playRound(cc,uc){
     let strC;
     if (cc===1)
@@ -14,22 +10,58 @@ function playRound(cc,uc){
         strC="paper";
     else if (cc===3)
         strC="scissor";
-    
-    if (uc===strC)
-        return "It was a tie.";
-    else if (uc==="rock"&&strC==="paper")
-        return "Computer won! Paper beats rock";
-    else if (uc==="rock"&&strC==="scissor")
-        return `You win rock beats scissor`;
-    else if (uc==="paper"&&strC==="scissor")
-        return "You lose! Scissor beats paper!";
-    else if (uc==="paper"&&strC==="rock")
-        return "You win! paper beats rock!";
-    else if (uc==="scissor"&&strC==="paper")
-        return "You win! scissor beats paper";
-    else if (uc==="scissor"&&strC==="rock")
-        return "You lose! rock beats scissor";
-    
+
+     if (uc==="rock"&&strC==="paper"){
+        console.log("Computer won! Paper beats rock") ;
+        return false;
+    }
+    else if (uc==="rock"&&strC==="scissor"){
+        console.log( `You win rock beats scissor`);
+        return true;
+    }
+    else if (uc==="paper"&&strC==="scissor"){
+        console.log("You lose! Scissor beats paper!");
+        return false;
+    }
+    else if (uc==="paper"&&strC==="rock"){
+        console.log ("You win! paper beats rock!");
+        return true;
+    }
+    else if (uc==="scissor"&&strC==="paper"){
+        console.log( "You win! scissor beats paper");
+        return true;
+    }
+    else if (uc==="scissor"&&strC==="rock"){
+        console.log ("You lose! rock beats scissor");
+        return false;
+    }
 }
 
-console.log(playRound(cc,uc));
+
+function game(){
+    
+    let uCount=0, cCount=0;
+    for (i =0;i<5;++i){
+        let cc=getComputerChoice();
+        let uc = prompt("Enter your choice");
+        uc = uc.toLowerCase();
+        if (cc===uc){
+            console.log("IT was a tie.");
+            uCount++;
+            cCount++;
+        }
+        else{
+            let x = console.log(playRound(cc,uc));
+            if (x)
+                uCount++;
+            else
+                cCount++;
+        }
+    }
+    if (uCount>cCount)
+        console.log("YOU WON THE GAME");
+    else
+        console.log("YOU LOST THE GAME");
+}
+
+game();
